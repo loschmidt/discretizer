6c3a4c42722d3b92fbf8134288f463d125fbf5d2
Jan Stourac <stourac.jan@gmail.com>
2021-04-21 16:26:17 +0200

    change license to LGPL

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 # https://github.com/loschmidt/discretizer
 #
 # Discretizer is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Discretizer is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with Discretizer.  If not, see <https://www.gnu.org/licenses/>.
 
 import os, sys
@@ -164,14 +164,14 @@ setuptools.setup(
     version=main_ns['__version__'],
     author="Jan Plhak",
     author_email="408420@mail.muni.cz",
-    description="Tool used to transform tunnel in protein to a sequence of disks",
+    description="Tool for transformation of protein tunnels represented by spheres to a sequence of disks",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/loschmidt/discretizer",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
         "Environment :: Console",
     ],
